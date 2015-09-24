9a07558ba1a1fd567b72666d43a1f227adb5c8d2
Francis Brito <fr.br94@gmail.com>
2015-09-24 19:30:32 -0400

    Add adapter#find method placeholder.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,18 @@
 import libdebug from 'debug';
 
 // create debug logger
-const debug = libdebug('package-name');
+const debug = libdebug('storage-adapter');
 
-debug('it works!');
+const PROTOTYPE = {
+  find() {},
+};
+
+export default function createAdapter(options) {
+  debug('passed-in options (keys only): %j', Object.keys(options));
+
+  return Object.create(PROTOTYPE, {
+    storage: {
+      value: options.storage,
+    },
+  });
+}
