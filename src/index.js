612ea2781ca63f3fbf907078c4468683b2018d65
Francis Brito <fr.br94@gmail.com>
2015-09-25 06:48:22 -0400

    Add storage rejection logic.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /**
  * External imports
  */
+import _ from 'underscore';
 import mingo from 'mingo';
 import libdebug from 'debug';
 
@@ -18,6 +19,11 @@ export const ADAPTABLE_METHODS = [
   'aggregate',
 ];
 
+function isAdaptable(storage) {
+  return typeof storage === 'object'
+  && _.every(ADAPTABLE_METHODS, (method) => method in storage);
+}
+
 const PROTOTYPE = {
   find(query, projection) {
     debug('received query %j', query);
@@ -29,8 +35,12 @@ const PROTOTYPE = {
 
 export default function createAdapter(options = {}) {
   debug('passed-in options (keys only): %j', Object.keys(options));
+  debug('passed-in storage (type): %j', typeof options.storage);
 
   if (!options.storage) throw new Error('`options.storage` is missing.');
+  if (!(_.isArray(options.storage) || isAdaptable(options.storage))) {
+    throw new Error('`options.storage` should be an adaptable or an array.');
+  }
 
   return Object.create(PROTOTYPE, {
     storage: {
