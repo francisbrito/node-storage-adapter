bfee5830ada245b782ea37d7414df2f1404c07f3
Francis Brito <fr.br94@gmail.com>
2015-09-24 20:26:40 -0400

    Update adapter factory to throw if no storage is given.

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const PROTOTYPE = {
 export default function createAdapter(options = {}) {
   debug('passed-in options (keys only): %j', Object.keys(options));
 
+  if (!options.storage) throw new Error('Storage is missing.');
+
   return Object.create(PROTOTYPE, {
     storage: {
       value: options.storage,
